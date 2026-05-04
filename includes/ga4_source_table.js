4ae095946337f10e3451a19a2ce8c79e5aaec349
Gleb Baraniuk <g.b.baranyuk@gmail.com>
2026-05-04 21:06:21 +0000

    chore: remove unused ga4_source_table.js include

--- includes/ga4_source_table.js
+++
@@ -1,7 +0,0 @@
-function ga4EventsTable() {
-  const project = dataform.projectConfig.vars.ga4_project || 'bigquery-public-data';
-  const dataset = dataform.projectConfig.vars.ga4_dataset || 'ga4_obfuscated_sample_ecommerce';
-  return `\`${project}.${dataset}.events_*\``;
-}
-
-module.exports = { ga4EventsTable };
